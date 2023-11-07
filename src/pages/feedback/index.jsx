2732b06dc4b32b6f914af542dffa5c104fe096f1
George <cgeorgesavvas@gmail.com>
2023-11-07 06:59:58 +0000

    wip

--- src/pages/feedback/index.jsx
+++ src/pages/feedback/index.jsx
@@ -26,7 +26,7 @@ const Feedback = () => {
   const clientId = "d0ff51532a5a4c80514e181763912fd4c3edbf5fae07b5c5b9f6d18c545022fc";
   const redirectUri = "http://localhost:3000/feedback";
   const stateHash = "YOUR_UNIQUE_STATE_HASH";
-  const url = `https://gitlab.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&state=${stateHash}`;
+  const url = `http://gitlab.etc.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&state=${stateHash}`;
   const handleGitlabClick = () => {
     window.open(url, "_blank", "noreferrer");
   };
