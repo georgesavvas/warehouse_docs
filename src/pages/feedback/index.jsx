08b2dfc3f0b00757e76c8bd223c996b4a54b9b0c
George <cgeorgesavvas@gmail.com>
2023-11-07 06:37:49 +0000

    wip

--- src/pages/feedback/index.jsx
+++ src/pages/feedback/index.jsx
@@ -4,6 +4,7 @@ import {default as FeedbackLayout} from './Layout';
 import GlobalStyles from "@mui/material/GlobalStyles";
 import Layout from '@theme/Layout';
 import React from 'react';
+import { Typography } from "@mui/material";
 import darkScrollbar from "@mui/material/darkScrollbar";
 import styles from "./index.module.css";
 
@@ -22,11 +23,23 @@ const darkTheme = createTheme({
 });
 
 const Feedback = () => {
+  const clientId = "d0ff51532a5a4c80514e181763912fd4c3edbf5fae07b5c5b9f6d18c545022fc";
+  const redirectUri = "http://localhost:3000/feedback";
+  const stateHash = "YOUR_UNIQUE_STATE_HASH";
+  const url = `https://gitlab.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&state=${stateHash}`;
+  const handleGitlabClick = () => {
+    window.open(url, "_blank", "noreferrer");
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles styles={{ ...darkScrollbar() }} />
       <Layout title="Feedback">
-        <FeedbackLayout />
+        <div className={styles.button} onClick={handleGitlabClick}>
+          <img src="img/gitlab.png" style={{objectFit: "contain", height: "100%"}} />
+          <Typography>Sign in with gitlab</Typography>
+        </div>
+        {/* <FeedbackLayout /> */}
       </Layout>
     </ThemeProvider>
   )
