4370746772bd4c7b5fdbd025f5eac9bb49b59fbb
George Savvas <cgeorgesavvas@gmail.com>
2023-11-09 16:20:07 +0000

    wip

--- src/pages/feedback/Editor.jsx
+++ src/pages/feedback/Editor.jsx
@@ -1,21 +1,14 @@
-import React, {useState, useEffect, useRef} from "react";
-import {Dialog, DialogTitle, DialogContent, Button, Typography, TextField} from "@mui/material";
-import {serverRequest} from "../../services";
-import styles from "./Editor.module.css";
-import ReactQuill from "react-quill";
-import {useSnackbar} from "notistack";
 import "react-quill/dist/quill.bubble.css";
 import "./quill.css";
 
-const dialogStyle = {
-  "& .MuiDialog-container": {
-    "& .MuiPaper-root": {
-      height: "50vh",
-      backgroundColor: "rgb(25, 25, 25)",
-      backgroundImage: "none",
-    },
-  },
-};
+import {Button, Dialog, DialogContent, DialogTitle, TextField, Typography} from "@mui/material";
+import React, {useEffect, useRef, useState} from "react";
+
+import ReactQuill from "react-quill";
+import {serverRequest} from "../../services";
+import styles from "./Editor.module.css";
+import {useSnackbar} from "notistack";
+import { v4 as uuidv4 } from 'uuid';
 
 const Editor = props => {
   const editorRef = useRef();
@@ -23,6 +16,8 @@ const Editor = props => {
   const [titleValue, setTitleValue] = useState("");
   const [contentValue, setContentValue] = useState("");
   const [commentValue, setCommentValue] = useState("");
+  const [tags, setTags] = useState([]);
+  const [canUpdate, setCanUpdate] = useState(false);
   const {enqueueSnackbar} = useSnackbar();
   const {created, user, content, comments, status} = props.entry || {};
 
@@ -39,9 +34,20 @@ const Editor = props => {
     if (!editMode) return;
     setTitleValue(content?.title || "");
     setContentValue(content?.description || "");
+    setTags(content?.tags || []);
   }, [editMode]);
 
   const isNew = !props.entry?.id;
+
+  const dialogStyle = {
+    "& .MuiDialog-container": {
+      "& .MuiPaper-root": {
+        height: isNew ? "50vh" : "80vh",
+        backgroundColor: "rgb(25, 25, 25)",
+        backgroundImage: "none",
+      },
+    },
+  };
 
   const readerIsAuthor = props.reader === user;
   const column = props.columns.find(column => column.status === status);
@@ -79,9 +85,21 @@ const Editor = props => {
   }
 
   const handleCommentPost = () => {
-    serverRequest("get_posts", {kind: "rfe", service: "warehouse"}).then(resp => {
-      setData([...resp.data, ...extraData]);
+    const data = {
+      id: props.entry.id,
+      user: props.reader,
+      comment: commentValue,
+    }
+    serverRequest("create_comment", data).then(resp => {
+      if (!resp.ok) enqueueSnackbar("There was an issue posting your comment :(", {variant: "error"});
+    })
+    props.setData(prev => {
+      const existing = [...prev];
+      const post = existing.find(post => post.id === props.entry.id);
+      post.comments.push({id: uuidv4(), user: props.reader, content: commentValue});
+      return existing;
     });
+    setCommentValue("");
   };
 
   const renderCommentEditor = () => {
@@ -107,21 +125,28 @@ const Editor = props => {
               }
             </>
           )}
-        </div>
-        <Typography variant="h6" sx={{mt: 2}}>New Comment</Typography>
-        <div className={styles.quillContainer}>
-          <ReactQuill value={commentValue} onChange={setCommentValue} />
-        </div>
-        <div className={styles.postContainer}>
-          <Button
-            disabled={!commentValue}
-            onClick={handleCommentPost}
-            variant="outlined"
-            color="success"
-            sx={{minWidth: "150px"}}
-          >
-            Post
-          </Button>
+          <div data-color-mode="dark" className={`editor-container ${styles.editorCommentContainer}`}>
+            <ReactQuill
+              ref={editorRef}
+              className={styles.markdownEditor}
+              theme="bubble"
+              bounds=".editor-container"
+              value={commentValue}
+              placeholder="New Comment"
+              onChange={setCommentValue}
+            />
+            <div className={styles.postContainer}>
+              <Button
+                disabled={!commentValue}
+                onClick={handleCommentPost}
+                variant="outlined"
+                color="success"
+                sx={{minWidth: "150px"}}
+              >
+                Post
+              </Button>
+            </div>
+          </div>
         </div>
       </DialogContent>
     );
@@ -133,6 +158,7 @@ const Editor = props => {
       service: "warehouse",
       user: props.reader,
       status: "todo",
+      tags: tags,
       content: {
         title: titleValue,
         description: contentValue,
@@ -142,6 +168,34 @@ const Editor = props => {
       if (resp.ok) enqueueSnackbar("Feedback created!", {variant: "success"});
       else enqueueSnackbar("There was an issue creating your post :(", {variant: "error"});
     })
+    props.setData(prev => [...prev, {...data, id: uuidv4(), comments: []}]);
+    props.onClose();
+    setTitleValue("");
+    setContentValue("");
+  };
+
+  const handleUpdate = () => {
+    const data = {
+      id: props.entry.id,
+      kind: "rfe",
+      service: "warehouse",
+      status: "todo",
+      tags: tags,
+      content: {
+        title: titleValue,
+        description: contentValue,
+      },
+    }
+    serverRequest("create_post", data).then(resp => {
+      if (resp.ok) enqueueSnackbar("Feedback created!", {variant: "success"});
+      else enqueueSnackbar("There was an issue creating your post :(", {variant: "error"});
+    })
+    props.setData(prev => {
+      const existing = [...prev];
+      const index = existing.findIndex(post => post.id === props.entry.id);
+      existing[index] = data;
+      return existing;
+    });
     props.onClose();
     setTitleValue("");
     setContentValue("");
@@ -171,12 +225,17 @@ const Editor = props => {
         </Typography>}
         {renderCommentEditor()}
         <div className={styles.buttonContainer}>
+          <Button color="secondary" variant="outlined" size="small" onClick={props.onClose}>Close</Button>
           {isNew &&
             <Button color="success" variant="outlined" size="small" onClick={handleCreate}>
               Create
             </Button>
           }
-          <Button color="secondary" variant="outlined" size="small">Close</Button>
+          {!isNew &&
+            <Button color="success" variant="outlined" size="small" onClick={handleUpdate}>
+              Update
+            </Button>
+          }
         </div>
       </div>
     </Dialog>
